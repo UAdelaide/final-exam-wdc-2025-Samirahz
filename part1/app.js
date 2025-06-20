e2ed8ecc4fd08f20a9fe02f3188739ae929cfef8
Samira <a1852922@adelaide.edu.au>
2025-06-20 00:58:01 +0000

    [automatic commit] 2025-06-20 00:58:01 UTC 121.44.208.60

--- part1/app.js
+++ part1/app.js
@@ -16,6 +16,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 /*app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter);*/
+
+let db;
+
 
 module.exports = app;
