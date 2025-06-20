fb5c1d62871d2e41ea8341401780bde4b5d30076
Samira <a1852922@adelaide.edu.au>
2025-06-20 06:23:45 +0000

    [automatic commit] 2025-06-20 06:23:45 UTC 121.44.208.60

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const session = require('express-session');  // added this for part2
+const session = require('express-session');  // added this for session middleware
 require('dotenv').config();
 
 const app = express();
