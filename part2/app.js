e09f10287c8e7b6669ba7b9dade1a0df6d81703a
Samira <a1852922@adelaide.edu.au>
2025-06-20 04:09:41 +0000

    [automatic commit] 2025-06-20 04:09:41 UTC 121.44.208.60

--- part2/app.js
+++ part2/app.js
@@ -81,6 +81,14 @@ app.get('/api/users/my-dogs', async (req, res) => {
   }
 });
 
+// added for task16
+app.get('/api/users/me', (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).json({ error: 'Not logged in' });
+  }
+  res.json(req.session.user); // returns { id, role }
+});
+
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
